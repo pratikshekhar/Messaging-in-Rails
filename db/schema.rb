09aac107d440a8125ec86b457aee98601512495a
Pratik Shekhar <pratikshekhar26@gmail.com>
2017-12-05 05:21:01 +0100

    All associations setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205034644) do
+ActiveRecord::Schema.define(version: 20171205041542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipients", force: :cascade do |t|
+    t.integer "message_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171205034644) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :message do
+    body "MyText"
+    sender_id 1
+  end
+end
