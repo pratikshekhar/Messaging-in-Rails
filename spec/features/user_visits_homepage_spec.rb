2d450dbfb95458b5c16870eb2421d5030ff5dd6a
Pratik Shekhar <pratikshekhar26@gmail.com>
2017-12-05 07:21:09 +0100

    Messaging applications complete

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 feature 'user visits homepage' do
 	scenario ' successfully' do
+	    create_message('hello')
 		visit root_path
 
-		expect(page).to have_css 'h1'
+		expect(page).to have_css '.message'
 	end
 end
