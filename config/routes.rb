4a1592bed34e64437def9d76bb5bf802129d31fb
Pratik Shekhar <pratikshekhar26@gmail.com>
2017-12-05 06:36:05 +0100

    Application ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-    root 'static_pages#root'
+    root 'messages#index'
+    resources :messages, only: [:create, :destroy, :index, :new]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do |n|
+  name  = Faker::Internet.user_name
+  email = "example-#{n+1}@example.org"
+  password = "password"
+  User.create!(username:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
